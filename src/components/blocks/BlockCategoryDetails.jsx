8d634f0d86408245709133e77a2b563c193d0c4c
WT <whteoh1352@gmail.com>
2021-07-09 15:16:50 +0800

    fliter panel

--- src/components/blocks/BlockCategoryDetails.jsx
+++ src/components/blocks/BlockCategoryDetails.jsx
@@ -6,9 +6,18 @@ import { Link } from "react-router-dom";
 
 // blocks
 import BlockSlideShow from '../blocks/BlockSlideShow';
-
+import TextField from '@material-ui/core/TextField';
 // components
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
+import LocalShippingOutlinedIcon from '@material-ui/icons/LocalShippingOutlined';
+import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
+import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined';
+import Button from '@material-ui/core/Button';
+
+// styles
+import './styles/BlockCategoryDetails.css'
 
 function mapStateToProps(state) {
     return {
@@ -25,7 +34,7 @@ function mapDispatchToProps(dispatch) {
 const initialState = {
     selectedCategory: [],
     productSubCategories: [],
-
+    selectedSubCategory: ""
 }
 
 class BlockCategoryDetails extends Component {
@@ -48,7 +57,10 @@ class BlockCategoryDetails extends Component {
             try {
                 if (selectedCategory.length > 0) {
                     let subCategories = JSON.parse(selectedCategory[0].HierarchyItem)
-                    console.log(subCategories)
+                    this.setState({
+                        productSubCategories: subCategories,
+                        selectedSubCategory: (subCategories.length > 0) ? subCategories[0].ProductCategory : ""
+                    })
                 }
                 else {
                     this.setState({
@@ -84,8 +96,61 @@ class BlockCategoryDetails extends Component {
                                 <div style={{ cursor: "pointer", fontWeight: 600 }}>
                                     <FormatListBulletedIcon /> {" "} All Categories
                                 </div>
-                                <div>
+                                <div style={{ fontSize: '10pt' }}>
+                                    {
+                                        this.state.productSubCategories.map((el, idx) => {
+                                            return (
+                                                this.state.selectedSubCategory === el.ProductCategory ?
+                                                    <div key={el.ProductCategory} className="sub-category-items active">
+                                                        <DoubleArrowIcon fontSize='sm' /> {el.ProductCategory}
+                                                    </div>
+                                                    :
+                                                    <div key={el.ProductCategory} className="sub-category-items">
+                                                        <FiberManualRecordOutlinedIcon fontSize='sm' /> {el.ProductCategory}
+                                                    </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
 
+                            <div className="filtering-segment mt-3">
+                                <div className="location-segment">
+                                    <div><LocalShippingOutlinedIcon /> SHIPPED FROM</div>
+
+                                    <div>
+
+                                    </div>
+                                </div>
+
+                                <div className="prices-segment mt-3">
+                                    <div><MonetizationOnOutlinedIcon /> PRICE</div>
+
+                                    <div className="d-flex w-75">
+                                        <TextField className="mr-auto" label="MIN" variant="outlined" size="small" style={{ width: 100, height: 40, fontSize: '8pt' }} ></TextField>
+                                        <span className="mx-2 my-auto"> - </span>
+                                        <TextField className="ml-auto" label="MAX" variant="outlined" size="small" style={{ width: 100, height: 40, fontSize: '8pt' }} ></TextField>
+
+                                    </div>
+                                    <Button variant="contained" color="primary" disableElevation className="w-75" style={{ backgroundColor: 'rgb(153, 188, 59)' }}>
+                                        Filter Price
+                                    </Button>
+                                </div>
+
+                                <div className="brand-segment mt-3">
+                                    <div><LocalOfferOutlinedIcon /> BRANDS</div>
+
+                                    <div>
+
+                                    </div>
+                                </div>
+
+                                <div className="rating-segment mt-3">
+                                    <div><LocalOfferOutlinedIcon /> PROMOTION</div>
+
+                                    <div>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
